7e4846a4c739cea10875719daa50d7ff3c6a0bd0
Sean Reid <seanreid.mail@gmail.com>
2026-05-23 20:56:48 -0700

    feat(game): widen username dictionaries to ~4M combinations
    Grows the adjective and noun lists to 64 entries each, keeping the
    silly clown/mime tone. New combinations() helper exposes the domain
    size so other code (and tests) can reason about collision risk.

--- game/scripts/username_generator.gd
+++ game/scripts/username_generator.gd
@@ -1,11 +1,20 @@
 extends Node
 
-## Procedural username generator. Pairs an adjective with a clown/mime noun.
+## Procedural username generator. Pairs a silly adjective with a clown/mime
+## themed noun and a three-digit suffix. Domain size: ADJECTIVES x NOUNS x 1000.
 
 const ADJECTIVES := [
 	"Silent", "Painted", "Loud", "Floppy", "Crooked", "Bashful", "Velvet",
 	"Hushed", "Ruffled", "Striped", "Glossy", "Pale", "Sneaky", "Whiskered",
 	"Brittle", "Tipsy", "Polka", "Wobbly", "Crinkled", "Powdered",
+	"Squeaky", "Tufted", "Knobbly", "Frilly", "Wonky", "Boggled",
+	"Plucky", "Drooping", "Frazzled", "Soggy", "Greasy", "Jaunty",
+	"Saggy", "Lopsided", "Petite", "Stilted", "Drippy", "Mottled",
+	"Googly", "Spangled", "Befuddled", "Twitchy", "Mournful", "Limpid",
+	"Smudged", "Curdled", "Cracked", "Bumbling", "Threadbare", "Daffy",
+	"Dusty", "Garish", "Hapless", "Wistful", "Brassy", "Maudlin",
+	"Frumpy", "Yawning", "Stooped", "Vexed", "Spotty", "Lurching",
+	"Foggy", "Crumbly",
 ]
 
 const NOUNS := [
@@ -13,6 +22,14 @@ const NOUNS := [
 	"Marceau", "Tramp", "Auguste", "Whiteface", "Carnie", "Pagliacci",
 	"Punchinello", "Hopo", "Cake", "Honk", "Greasepaint", "Stripes",
 	"Tear", "Glove",
+	"Wig", "Nose", "Shoe", "Banana", "Pinwheel", "Smile",
+	"Frown", "Lapel", "Pocket", "Hatband", "Cravat", "Suspender",
+	"Bowtie", "Trumpet", "Kazoo", "Squirt", "Ladder", "Bucket",
+	"Pratfall", "Cartwheel", "Tightrope", "Unicycle", "Confetti", "Crumpet",
+	"Boutonniere", "Mistletoe", "Marionette", "Glissando", "Pantomime", "Vaudeville",
+	"Curtain", "Footlight", "Spotlight", "Soliloquy", "Pirouette", "Maskmaker",
+	"Topiary", "Sashay", "Gambol", "Tumbler", "Patter", "Whoopee",
+	"Mime", "Clown",
 ]
 
 func generate() -> String:
@@ -20,3 +37,6 @@ func generate() -> String:
 	var noun: String = NOUNS[randi() % NOUNS.size()]
 	var num := randi() % 1000
 	return "%s%s%03d" % [adj, noun, num]
+
+func combinations() -> int:
+	return ADJECTIVES.size() * NOUNS.size() * 1000
